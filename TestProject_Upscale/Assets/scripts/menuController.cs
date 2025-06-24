83c13c9c3e69f3e90013bac01648fad208c9fa98
Nikita_Kukharskiy <kukharskiy.creator@mail.ru>
2025-06-24 23:22:51 +0300

    [TestProject_Upscale] create main menu screen

--- TestProject_Upscale/Assets/scripts/menuController.cs
+++ TestProject_Upscale/Assets/scripts/menuController.cs
@@ -5,8 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class menuController : MonoBehaviour
 {
-    public void playGame()
+    public void menuGame()
     {
         SceneManager.LoadSceneAsync(1);
+    }
+    public void playGame()
+    {
+        SceneManager.LoadSceneAsync(2);
+    }
+    public void settingsGame()
+    {
+        SceneManager.LoadSceneAsync(3);
+    }
+    public void loadGame()
+    {
+        SceneManager.LoadSceneAsync(4);
+    }
+    public void creditsGame()
+    {
+        SceneManager.LoadSceneAsync(5);
+    }
+    public void quitGame()
+    {
+        Application.Quit();
     }
 }
